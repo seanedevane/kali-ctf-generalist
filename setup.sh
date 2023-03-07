d1b603e3ce47e55e21b1bee4f50a711f281a1fb5
Sean Edevane <sean@muimedia.com>
2023-03-06 22:42:40 -0500

    Initial commit Added setup.sh to handle getting pip installed, Ansible, and some small setup steps for later.

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Exit immediately on failure
+
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as sudo or root."
+    exit 1
+fi
+
+echo "Installing the CTF Generalist Kali build."
+
+echo "--------------------------------------------"
+echo "              Installing pip                "
+echo "--------------------------------------------"
+
+# Install pip before we do anything else
+curl -sL https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python3 /tmp/get-pip.py --user --no-warn-script-location
+
+export PATH=$PATH:/root/.local/bin
+
+echo "--------------------------------------------"
+echo "              Installing Ansible            "
+echo "--------------------------------------------"
+
+pip install ansible
+
+# Setup fonts
+mkdir ~/Scripts
+cd ~/Scripts
+git clone https://github.com/powerline/fonts
+cd fonts
+chmod +x install.sh
+./install.sh
+cd ~
+
+# Setup Terminator
+mkdir ~/.config/terminator
+cp ./terminatorconfig ~/.config/terminator/config
