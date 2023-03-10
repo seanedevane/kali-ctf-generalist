2613a937aaf01a5946fad7bb4235043aca4f740e
Sean Edevane <sean@muimedia.com>
2023-03-10 16:27:42 -0500

    Confirmed working deploy.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Exit immediately on failure
-
-set -e
-
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as sudo or root."
     exit 1
@@ -26,17 +22,6 @@ echo "              Installing Ansible            "
 echo "--------------------------------------------"
 
 pip3 install ansible
-
-echo "Setting up fonts..."
-# Setup fonts
-mkdir ~/Scripts
-pushd ~/Scripts > /dev/null
-git clone https://github.com/powerline/fonts
-popd > /dev/null
-pushd ~/Scripts/fonts > /dev/null
-chmod +x install.sh
-./install.sh
-popd > /dev/null
 
 echo "--------------------------------------------"
 echo "          Running Ansible playbook          "
